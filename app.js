07f05b9f64d1655867dd2609dea677b9dd41cf96
Rayster Ramos <rjrfrst@gmail.com>
2023-11-28 18:10:22 +0000

    MVP complete

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
 const getCountryByName = (countryName) => {
-
+    //Fetch data 
+    let country = "";
     fetch (`https://restcountries.com/v3.1/name/${countryName}`)
     .then ((response) => {
         return response.json();
     })
 
     .then ((data) => {
-        console.log(data);
+        country = data[0];
+        console.log(country);
+    
+
+    const displayCountryDetails = document.querySelector("#country-info"); //get the section
+    //Get attributes: name, capital, languages, population 
+
+    //Create empty element for an attribute: name
+    //Add a text inside the element
+    //Append the text in the section
+    const name = document.createElement('h2');
+    name.innerText = `Country Name: ${country.name.common}`;
+    displayCountryDetails.appendChild(name);
+
+    const population = document.createElement('p');
+    population.innerText = `population: ${country.population}`;
+    displayCountryDetails.appendChild(population);
+
+    const languages = document.createElement('p');
+    let countryLanguages = Object.values(country.languages); //Change the object to arrays
+    languages.innerText = `languages: ${countryLanguages}`; 
+    displayCountryDetails.appendChild(languages);
+
+    const capital = document.createElement('p');
+    capital.innerText = `capital: ${country.capital}`;
+    displayCountryDetails.appendChild(capital);
+
     })
 
+
 }
-getCountryByName("France"); //Calling the function
-// console.log(getCountryByName);
+getCountryByName("Switzerland"); //Call the function
+
+
+
+
+
+
+
